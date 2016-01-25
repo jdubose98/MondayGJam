dc25f8471b37cfd845b1707c828c86ebefefcf4a
Jacob Dubose <jdubose@IT405.mr.preble.com>
2016-01-25 09:23:50 -0500

    Enemy added

---
+++ 2dPlatformer/Assets/DumbEnemyScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DumbEnemyScript : MonoBehaviour {
+
+    [SerializeField] Transform Player;
+    [SerializeField] float MaxChaseRange;
+    [SerializeField] float MaxMoveSpeed;
+    [SerializeField] float JumpPower;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Mathf.Abs(Player.position.x - gameObject.transform.position.x) <= MaxChaseRange)
+        {
+            Debug.Log("In range");
+            if (Player.position.x - gameObject.transform.position.x > 0)
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(MaxMoveSpeed, 0));
+            }
+            else if (Player.position.x - gameObject.transform.position.x < 0)
+                {
+                    gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(new Vector2(-MaxMoveSpeed, 0));
+            }
+        }
+	}
+}
---
+++ 2dPlatformer/Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour 
+{
+    [SerializeField] float OffsetX;
+    [SerializeField] float OffsetY;
+
+	public Transform player;
+	void Update () 
+	{
+		transform.position = new Vector3 (player.position.x + OffsetX, player.position.y + OffsetY, -10);
+	}
+}
